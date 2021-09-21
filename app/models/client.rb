48a1d5c89e3d5db464028e9f832be18eef7bc5e0
Diego Novais <contato@diegonovais.com.br>
2021-09-21 15:25:22 -0300

    test client when all data is empty

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,13 @@
 class Client < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }, uniqueness: true
+  validates :cpf, presence: true, uniqueness: true
+
+  validate :cpf, :check_cpf_valid
+
+  private
+
+  def check_cpf_valid
+    errors.add(:cpf, "the cpf: #{cpf} is not valid") unless CPF.valid?(cpf)
+  end
 end
--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'when all data is empty' do
+    # Arrange
+    client = Client.new
+
+    # Act
+    client.valid?
+
+    # Assert
+    assert client.errors.present?
+
+    client_errors = client.errors.as_json.transform_values { |value| value.join(', ') }
+
+    assert_equal("can't be blank", client_errors[:name])
+    assert_equal("can't be blank, is invalid", client_errors[:email])
+    assert_equal("can't be blank, the cnpj:  is not valid", client_errors[:cnpj])
+  end
 end
