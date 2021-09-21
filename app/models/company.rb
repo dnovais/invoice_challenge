617415c716364eee37a26c1113f286a035193d1b
Diego Novais <contato@diegonovais.com.br>
2021-09-21 13:55:53 -0300

    when already exists a company with email

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,6 @@
 
 class Company < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }, uniqueness: true
   validates :cnpj, presence: true
 end
--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -17,4 +17,42 @@ class CompanyTest < ActiveSupport::TestCase
     assert_equal("can't be blank, is invalid", company_errors[:email])
     assert_equal("can't be blank", company_errors[:cnpj])
   end
+
+  test 'when format of company email is not valid' do
+    # arrange
+    name = 'SOS ME'
+    email = 'a@a'
+    cnpj = '75.692.217/0001-09'
+    address = 'Via de Pedestre Eurídice 896 Jardim Brasil (Zona Norte) São Paulo SP'
+    phone = '(11) 2677-8628'
+
+    # act
+    company = Company.create(name: name, email: email, cnpj: cnpj, address: address, phone: phone)
+
+    # assert
+    assert company.errors.present?
+
+    company_errors = company.errors.as_json.transform_values { |value| value.join(', ') }
+
+    assert_equal('is invalid', company_errors[:email])
+  end
+
+  test 'when already exists a company with email' do
+    # arrange
+    name = 'Other Company ME'
+    email = companies(:one).email
+    cnpj = '75.692.217/0001-09'
+    address = 'Via de Pedestre Eurídice 896 Jardim Brasil (Zona Norte) São Paulo SP'
+    phone = '(11) 2677-8628'
+
+    # act
+    company = Company.create(name: name, email: email, cnpj: cnpj, address: address, phone: phone)
+
+    # assert
+    assert company.errors.present?
+
+    company_errors = company.errors.as_json.transform_values { |value| value.join(', ') }
+
+    assert_equal('has already been taken', company_errors[:email])
+  end
 end
