7908b6e5cd45c4606e2cb2da0ca3f67bd68a9f19
Diego Novais <contato@diegonovais.com.br>
2021-09-21 11:07:14 -0300

    test company when all data is empty

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Company < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :cnpj, presence: true
 end
--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class CompanyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'when all data is empty' do
+    # Arrange
+    company = Company.new
+
+    # Act
+    company.valid?
+
+    # Assert
+    assert company.errors.present?
+
+    company_errors = company.errors.as_json.transform_values { |value| value.join(', ') }
+
+    assert_equal("can't be blank", company_errors[:name])
+    assert_equal("can't be blank, is invalid", company_errors[:email])
+    assert_equal("can't be blank", company_errors[:cnpj])
+  end
 end
