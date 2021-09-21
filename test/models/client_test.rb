c56d22a80911997b09c63d57a4951f843f0fd09c
Diego Novais <contato@diegonovais.com.br>
2021-09-21 15:39:45 -0300

    test when format of company email is not valid

--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -15,6 +15,25 @@ class ClientTest < ActiveSupport::TestCase
 
     assert_equal("can't be blank", client_errors[:name])
     assert_equal("can't be blank, is invalid", client_errors[:email])
-    assert_equal("can't be blank, the cnpj:  is not valid", client_errors[:cnpj])
+    assert_equal("can't be blank, the cpf:  is not valid", client_errors[:cpf])
+  end
+
+  test 'when format of company email is not valid' do
+    # arrange
+    name = 'Agatha Beatriz da Mata'
+    email = 'a@a'
+    cpf = '033.704.441-43'
+    address = 'Rua Antônio Teixeira Morales'
+    phone = '(51) 3751-4077'
+
+    # act
+    client = Client.create(name: name, email: email, cpf: cpf, address: address, phone: phone)
+
+    # assert
+    assert client.errors.present?
+
+    client_errors = client.errors.as_json.transform_values { |value| value.join(', ') }
+
+    assert_equal('is invalid', client_errors[:email])
   end
 end
