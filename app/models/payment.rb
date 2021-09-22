6cefef7a4be90264186f998d78732c7ffca5b0b9
Diego Novais <contato@diegonovais.com.br>
2021-09-21 22:56:34 -0300

    test when payments payment_kind not exist on enum

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,13 @@
 class Payment < ApplicationRecord
   belongs_to :invoice
+
+  enum payment_kind: {
+    money: 0,
+    credit: 1,
+    debit: 2,
+    pix: 3
+  }
+
+  validates :amount, :payment_date, :status, presence: true
+  validates :payment_kind, inclusion: { in: payment_kinds.keys }, presence: true
 end
--- test/models/payment_test.rb
+++ test/models/payment_test.rb
@@ -12,4 +12,47 @@ class PaymentTest < ActiveSupport::TestCase
     # assert
     assert_equal payment_invoice, invoice
   end
+
+  test 'when all data is empty' do
+    # Arrange
+    invoice = invoices(:one)
+
+    payment = Payment.new
+    payment.invoice_id = invoice.id
+    payment.payment_kind = nil
+    payment.status = nil
+
+    # Act
+    payment.valid?
+
+    # Assert
+    assert payment.errors.present?
+
+    payment_errors = payment.errors.as_json.transform_values { |value| value.join(', ') }
+
+    assert_equal("can't be blank", payment_errors[:amount])
+    assert_equal("is not included in the list, can't be blank", payment_errors[:payment_kind])
+    assert_equal("can't be blank", payment_errors[:payment_date])
+    assert_equal("can't be blank", payment_errors[:status])
+  end
+
+  test 'when payments payment_kind not exist on enum' do
+    # arrange
+    invoice = invoices(:one)
+    amount = 6.00
+    payment_kind = 'payment_kind_enum_not_valid'
+    payment_date = Time.zone.today
+    status = 0
+
+    # act and assert
+    assert_raises ArgumentError do
+      Payment.create(
+        invoice: invoice,
+        amount: amount,
+        payment_kind: payment_kind,
+        payment_date: payment_date,
+        status: status
+      )
+    end
+  end
 end
