14b9a78fe4d1850cffaded3acf4f8ecb26344467
Diego Novais <contato@diegonovais.com.br>
2021-09-21 22:12:43 -0300

    test when invoices due_date is in the past

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,22 @@
 class Invoice < ApplicationRecord
   belongs_to :company
   belongs_to :client
+
+  enum status: {
+    open: 0,
+    closed: 1,
+    paid: 2
+  }
+
+  validates :title, presence: true
+  validates :status, presence: true, inclusion: { in: statuses.keys }
+  validates :due_date, presence: true
+
+  validate :due_date, :due_date_cannot_be_in_the_past
+
+  private
+
+  def due_date_cannot_be_in_the_past
+    errors.add(:due_date, "can't be in the past") if due_date.present? && due_date < Time.zone.today
+  end
 end
--- test/models/invoice_test.rb
+++ test/models/invoice_test.rb
@@ -15,4 +15,98 @@ class InvoiceTest < ActiveSupport::TestCase
     assert_equal invoice_client, client
     assert_equal invoice_company, company
   end
+
+  test 'when all data is empty' do
+    # Arrange
+    client = clients(:one)
+    company = companies(:one)
+
+    invoice = Invoice.new
+    invoice.client_id = client.id
+    invoice.company_id = company.id
+    invoice.status = nil
+
+    # Act
+    invoice.valid?
+
+    # Assert
+    assert invoice.errors.present?
+
+    invoice_errors = invoice.errors.as_json.transform_values { |value| value.join(', ') }
+
+    assert_equal("can't be blank", invoice_errors[:title])
+    assert_equal("can't be blank, is not included in the list", invoice_errors[:status])
+    assert_equal("can't be blank", invoice_errors[:due_date])
+  end
+
+  test 'when invoices status not exist on enum' do
+    # arrange
+    company = companies(:one)
+    client = clients(:one)
+    title = 'Invoice 01'
+    status = 'enum_not_valid'
+    description = 'Sample of description'
+    discount_percentage = 9.99
+    discount_amount = 9.99
+    tax_percentage = 9.99
+    tax_amount = 9.99
+    due_date = '2021-09-25'
+    sub_total = 9.99
+    total = 9.99
+
+    # act and assert
+    assert_raises ArgumentError do
+      Invoice.create(
+        company_id: company.id,
+        client_id: client.id,
+        title: title,
+        status: status,
+        description: description,
+        discount_percentage: discount_percentage,
+        discount_amount: discount_amount,
+        tax_percentage: tax_percentage,
+        tax_amount: tax_amount,
+        due_date: due_date,
+        sub_total: sub_total,
+        total: total
+      )
+    end
+  end
+
+  test 'when invoices due_date is in the past' do
+    # arrange
+    company = companies(:one)
+    client = clients(:one)
+    title = 'Invoice 01'
+    description = 'Sample of description'
+    discount_percentage = 9.99
+    discount_amount = 9.99
+    tax_percentage = 9.99
+    tax_amount = 9.99
+    due_date = Time.zone.today - 1.day
+    sub_total = 9.99
+    total = 9.99
+
+    # act
+    invoice = Invoice.create(
+      company_id: company.id,
+      client_id: client.id,
+      title: title,
+      description: description,
+      discount_percentage: discount_percentage,
+      discount_amount: discount_amount,
+      tax_percentage: tax_percentage,
+      tax_amount: tax_amount,
+      due_date: due_date,
+      sub_total: sub_total,
+      total: total
+    )
+
+    # assert
+    assert invoice.errors.present?
+
+    invoice_errors = invoice.errors.as_json.transform_values { |value| value.join(', ') }
+
+    assert_equal("can't be in the past", invoice_errors[:due_date])
+  end
 end
