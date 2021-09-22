201b22941780a98db8655f55e428dc3b957f3587
Diego Novais <contato@diegonovais.com.br>
2021-09-21 23:27:00 -0300

    test when all data is empty

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,3 @@
 class Item < ApplicationRecord
+  validates :name, :unit_cost, :quantity, presence: true
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'when all data is empty' do
+    # Arrange
+    item = Item.new
+
+    # Act
+    item.valid?
+
+    # Assert
+    assert item.errors.present?
+
+    item_errors = item.errors.as_json.transform_values { |value| value.join(', ') }
+
+    assert_equal("can't be blank", item_errors[:name])
+    assert_equal("can't be blank", item_errors[:unit_cost])
+    assert_equal("can't be blank", item_errors[:quantity])
+  end
 end
