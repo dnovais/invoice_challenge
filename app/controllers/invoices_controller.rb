83f738d753691cb4aef500110334f0a14746c79b
Diego Novais <contato@diegonovais.com.br>
2021-09-27 17:18:53 -0300

    Add bootstrap

---
+++ app/controllers/invoices_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class InvoicesController < ApplicationController
+  def welcome; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'invoices#welcome'
+
+  get 'welcome', to: 'invoices#welcome'
+
   namespace :api do
     resources :invoices, only: [:index, :show, :create, :update]
   end
