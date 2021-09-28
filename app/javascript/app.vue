85f542489d2bb72f5a08059ff69a8691d4ec1292
Diego Novais <contato@diegonovais.com.br>
2021-09-28 19:06:26 -0300

    add vue.js

---
+++ app/javascript/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "I am learning Vue.js"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
